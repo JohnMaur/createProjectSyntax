63e3eb9126e7727e90e4cb337e2a9045e6ddb450
John Maur <johnmaur8@gmai.com>
2024-10-07 22:05:20 +0800

    added web and mobile apps shortcut for new clean file

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,18 @@ const App = () => {
       <h1 className='text-3xl font-bold'>Webapp create project (React Vite)</h1>
       <p className='text-3xl'>npm create vite@latest</p>
       <br />
+
+      <h1 className='text-3xl font-bold'>Webapp shortcut new clean file</h1>
+      <p className='text-3xl'>rafce</p>
+      <br />
+
       <h1 className='text-3xl font-bold'>Mobile create project (React Native)</h1>
       <p className='text-3xl'>npx create-expo-app@latest --template blank</p>
+      <br />
+
+      <h1 className='text-3xl font-bold'>Mobile app shortcut new clean file</h1>
+      <p className='text-3xl'>rnfe</p>
+      <br />
     </div>
   )
 }
