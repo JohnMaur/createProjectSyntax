85c5effe3bddbf03717b6f1dd440776cc434a356
John Maur <johnmaur8@gmai.com>
2024-10-07 21:37:24 +0800

    Create new project syntax

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Webapp create project (React Vite)</h1>
+      <p className='text-3xl'>npm create vite@latest</p>
+      <br />
+      <h1 className='text-3xl font-bold'>Mobile create project (React Native)</h1>
+      <p className='text-3xl'>npx create-expo-app@latest --template blank</p>
+    </div>
+  )
+}
+
+export default App
